7328f7d016028596c6c3690d6e334c82d015fbe2
itsmichaelblack <michael@successtutoring.com>
2026-02-21 15:04:32 -0600

    Update main.jsx
    Find a location

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import FindACentre from './FindACentre.jsx'
+
+const path = window.location.pathname;
+
+function Root() {
+  if (path === '/find-a-centre' || path === '/find-a-center') {
+    return <FindACentre />;
+  }
+  return <App />;
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Root />
   </StrictMode>,
 )
