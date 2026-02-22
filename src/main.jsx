60c454fbd79b553e1f42fcaa43baeaa1dd5bde52
itsmichaelblack <michael@successtutoring.com>
2026-02-22 10:58:54 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import FindACentre from './FindACentre.jsx'
+import BookAssessment from './BookAssessment.jsx'
 
 const path = window.location.pathname;
 
 function Root() {
   if (path === '/find-a-centre' || path === '/find-a-center') {
     return <FindACentre />;
+  }
+  if (path === '/book-assessment' || path === '/book') {
+    return <BookAssessment />;
   }
   return <App />;
 }
